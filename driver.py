31e296ac1bae002f4b8fb625136952c44f066782
nowakmikolaj <nowak.mikolaj19@gmail.com>
2023-05-09 10:08:48 +0200

    Add makefile

--- driver.py
+++ driver.py
@@ -1,13 +1,11 @@
-import contextlib
 import sys
 
 from pyke import knowledge_engine
 from pyke import krb_traceback
 
-engine = knowledge_engine.engine(__file__)
-
 
 def run_driver():
+    engine = knowledge_engine.engine(__file__)
     engine.reset()
 
     engine.activate('rules')
@@ -18,13 +16,15 @@ def run_driver():
         with engine.prove_goal(
                 'rules.beer($beer)') as gen:  # STUDENTS: you will need to edit this line
             for vars, plan in gen:
-                print("You should drink: %s" % (vars['beer']))  # STUDENTS: you will need to edit this line
+                # STUDENTS: you will need to edit this line
+                print("You should drink: %s" % (vars['beer']))
 
     except Exception:
         # This converts stack frames of generated python functions back to the
         # .krb file.
         krb_traceback.print_exc()
         sys.exit(1)
+
 
 if __name__ == "__main__":
     run_driver()
